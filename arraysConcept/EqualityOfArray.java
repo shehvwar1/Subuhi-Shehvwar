6d9d3b001a45b4d6128d009e9c46dfae56731d16
subuhi shehvwar <shehvwars16@gmail.com>
2022-10-27 00:19:35 +0530

    20-10-22

---
+++ arraysConcept/EqualityOfArray.java
@@ -0,0 +1,105 @@
+package com.java.arraysConcept;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class EqualityOfArray {
+	/*public static void equalityOfArray()
+	{
+		Scanner sc = new Scanner(System.in);
+		int i,j;
+		int arr1[] = new int[5];
+		int arr2[] = new int[5];
+		System.out.println("Enter 5 array elements");
+		for( i=0; i<arr1.length;i++)
+		{
+			arr1[i] = sc.nextInt();
+		}*/
+		/*System.out.println("1st array elements are");
+		for( i=0; i<arr1.length;i++)
+		{
+			System.out.println(arr1[i]);
+		}*/
+		System.out.println("Enter 5 array elements");
+		for( j=0; j<arr2.length;j++)
+		{
+			arr1[j] = sc.nextInt();
+		}
+		/*if(arr1[i]== arr2[j])
+		{
+			System.out.println("arrays");
+		}
+		else
+		{
+			System.out.println("no array");
+		}*/
+		//System.out.println(Arrays.equals(arr1[i], arr2[j]));
+		/*System.out.println("2st array elements are");
+		for(j=0; j<arr2.length;j++)
+		{
+			System.out.println(arr1[j]);
+		}*/
+		/*boolean b = true;
+		if(arr1.length == arr2.length)
+		{
+			for( i =0; i<arr1.length;i++)
+			{
+				if(arr1[i] != arr2[i])
+				{
+					b = false;
+				}
+			}
+		}
+		else
+		{
+			b = false;
+		}
+		if(b == true)
+		{
+			System.out.println("Arrays are equal");
+		}
+		else
+		{
+			System.out.println("Arrays are not equal");
+		}*/
+		/*System.out.println("enter the values of 1st array");
+		int n = sc.nextInt();
+		System.out.println("enter the values of 2nd array");
+		int m = sc.nextInt();*/
+		/*int arr1[] = new int[5];
+		int arr2[] = new int[5];*/
+		/*System.out.println("reading input from user");
+		for(int i=0; i<n; i++)
+		{
+			arr1[i]=sc.nextInt();
+		}*/
+		/*System.out.println("1st array elements are");
+		for(int i=0; i<arr1.length;i++)
+		{
+			System.out.println(arr1[i]);
+		}*/
+		/*for(int j=0; j<m;j++)
+		{
+			arr2[j] = sc.nextInt();
+		}*/
+		/*for(int j=0; j<arr2.length;j++)
+		{
+			System.out.println(arr2[j]);
+		}*/
+		
+//	}
+		public static boolean equalityOfArray(int arr1[], int arr2[])
+		{
+			
+			
+			//final statement
+			return true;
+			
+		}
+
+	public static void main(String[] args) {
+		equalityOfArray();
+
+	}
+
+}
